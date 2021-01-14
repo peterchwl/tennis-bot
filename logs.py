4fd707fbcbc81937b2c6f24dfc9cdea0c2a72bcd
ryanlee68 <ryanlee619@gmail.com>
2021-01-13 22:19:08 -0800

    Finished up all logging

--- logs.py
+++ logs.py
@@ -15,19 +15,25 @@ def setUpLogger(loggers):
     logs_dir = pathlib.Path('logs')
     logs_dir.mkdir(exist_ok=True)
     # Create a handler that records all activity
-    everything = logging.handlers.TimedRotatingFileHandler(logs_dir / f'tennisbot.{format(datetime.datetime.today(), "%Y-%m-%d")}.log',
+    everything = logging.handlers.TimedRotatingFileHandler(logs_dir / f'ALL-LOGS.tennisbot.{format(datetime.datetime.today(), "%Y-%m-%d")}.log',
                                                            when='midnight', encoding='UTF-8')
     # Do not use loggging.NOTSET, does not work for some reason
     # use logging.DEBUG if you want the lowest level
     everything.setLevel(logging.DEBUG)
 
     # Create a handler that records only ERRORs and CRITICALs
-    errors_only = logging.handlers.TimedRotatingFileHandler(logs_dir / f'ERRORS.bdaybot.{format(datetime.datetime.today(), "%Y-%m-%d")}.log',
+    errors_only = logging.handlers.TimedRotatingFileHandler(logs_dir / f'ONLY-ERRORS-CRITICALS.tennisbot.{format(datetime.datetime.today(), "%Y-%m-%d")}.log',
                                                             when='midnight', encoding='UTF-8')
     errors_only.setLevel(logging.ERROR)
 
     # Rename files so .log is the file extension
     everything.namer, errors_only.namer = (file_renamer,) * 2
+    
+    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    everything.setFormatter(formatter)
+    errors_only.setFormatter(formatter)
+    
 
     # Add handlers to the logger
     logger.addHandler(everything)
@@ -39,4 +45,5 @@ def setUpLogger(loggers):
 
     # Add handler to the logger
     logger.addHandler(console)
+    
     
